e1284ebab04dc06a67a969063a13e923079b54bc
Luciano <lucianortiz97@gmail.com>
2022-07-30 17:07:17 -0300

    CORS

--- src/main/groovy/Controladores/TareaController.groovy
+++ src/main/groovy/Controladores/TareaController.groovy
@@ -8,12 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
+
+import javax.servlet.http.HttpServletResponse
 import java.time.LocalDate
 
-@CrossOrigin(origins = "*")
 @RestController
 class TareaController extends ApiControllerBase {
-
     @Autowired
     private TareaService service
 
--- src/main/groovy/app/Application.groovy
+++ src/main/groovy/app/Application.groovy
@@ -4,11 +4,17 @@ import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import springfox.documentation.builders.PathSelectors
@@ -17,74 +23,71 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+import javax.servlet.Filter
+import javax.servlet.FilterConfig
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @SpringBootApplication
+@ServletComponentScan
 @EnableMongoRepositories(basePackages = ["Repositorios"])
 @ComponentScan(basePackages = ["Controladores", "Servicios"])
 @Configuration
 @EnableSwagger2
 class Application {
 
-
 	static void main(String[] args) {
 		SpringApplication.run(Application.class, args)
 	}
 
-	@Bean MongoClient mongoClient() {
+	@Component
+	public class ConfigCtrl implements Filter {
+
+		@Override
+		public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+			final HttpServletResponse response = (HttpServletResponse) res;
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE, PATCH");
+			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			if ("OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+				response.setStatus(HttpServletResponse.SC_OK);
+			} else {
+				chain.doFilter(req, res);
+			}
+		}
+		@Override
+		public void destroy() {
+		}
+		@Override
+		public void init(FilterConfig config) throws ServletException {
+		}
+	}
+
+	@Bean
+	MongoClient mongoClient() {
 		return MongoClients.create("mongodb://localhost:27017")
 	}
 
-	@Bean MongoTemplate mongoTemplate() {
+	@Bean
+	MongoTemplate mongoTemplate() {
 		return new MongoTemplate(mongoClient(), "EasyScheduler")
 	}
 
-	@Bean Docket api() {
+	@Bean
+	Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
 				.build()
 	}
-
-	@Bean
-	WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000")
-			}
-		}
-	}
-
 }
-//@Configuration
-//@EnableWebSecurity
-//class WebSecurity implements SecurityFilterChain {
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.cors().and().csrf().disable()
-//				.authorizeRequests()
-//	}
-//
-//	@Bean
-//	CorsConfigurationSource corsConfigurationSource() {
-//		final CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-//		config.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
-//		config.setAllowCredentials(true);
-//		config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", config);
-//		return source;
-//	}
-//
-//	@Override
-//	boolean matches(HttpServletRequest request) {
-//		return false
-//	}
-//
-//	@Override
-//	List<Filter> getFilters() {
-//		return null
-//	}
-//}
+
+
